310b214f680b015b6118b82a8dff435e4e3fa354
Emmanuel Zuñiga Labra <emmanuel.zula@ciencias.unam.mx>
2025-03-06 09:24:38 +0000

    Generalizacion script

--- torchmd-net-train.sh
+++ torchmd-net-train.sh
@@ -24,17 +24,10 @@ echo "Entorno virtual 'torchmd-net-safe' activado."
 echo "Iniciando conversión de datos"
 # Ejecutar el script en Python
 python - <<EOF
-from LIACEI_workflow.analysis.DinamicaMolecular import Graficas_genericas
-from LIACEI_workflow.data.DinamicaMolecular import DinamicaMolecular, ElementoQuimico, Utils
+from LIACEI_workflow.data.DinamicaMolecular import DinamicaMolecular, ElementoQuimico
 from LIACEI_workflow.utils.utils import crear_carpeta
-from LIACEI_workflow.codes.torchmd_net.utils import (
-    cargar_desde_TorchMDNet,
-    generar_inferencias,
-    guardar_a_TorchMDNet,
-    guardar_metricas,
-    obtener_indices_test,
-    obtener_ruta_modelo_entrenado
-)
+from LIACEI_workflow.codes.torchmd_net.utils import guardar_a_TorchMDNet
+
 base_de_datos=DinamicaMolecular.cargar_desde_data("input/input.data")
 
 for frame in base_de_datos.frames.values():
@@ -44,12 +37,11 @@ for frame in base_de_datos.frames.values():
     # Restar la energía atómica total de la energía actual del frame
     frame.energia -= energia_total  # Actualizar directamente el atributo
 
-crear_carpeta("transform")  
+crear_carpeta("transform")
 
 guardar_a_TorchMDNet(base_de_datos, "transform/input_torchmd-net.h5")
 
 crear_carpeta("train")
-
 EOF
 
 if [[ $? -ne 0 ]]; then
@@ -87,6 +79,14 @@ echo "Entrenamiento finalizado correctamente en ${days} días, ${hours} horas, $
 echo "Iniciando generación de inferencias"
 
 python - <<EOF
+from LIACEI_workflow.data.DinamicaMolecular import DinamicaMolecular
+from LIACEI_workflow.codes.torchmd_net.utils import (
+    cargar_desde_TorchMDNet,
+    generar_inferencias,
+    obtener_indices_test,
+    obtener_ruta_modelo_entrenado
+)
+
 ruta_modelo = obtener_ruta_modelo_entrenado("train")
 
 base_de_datos = cargar_desde_TorchMDNet("transform/input_torchmd-net.h5")
@@ -111,6 +111,15 @@ echo "Generación de inferencias finalizada correctamente"
 echo "Iniciando generación de datos para graficar"
 
 python - <<EOF
+from LIACEI_workflow.data.DinamicaMolecular import Utils
+from LIACEI_workflow.data.DinamicaMolecular import DinamicaMolecular
+from LIACEI_workflow.utils.utils import crear_carpeta
+from LIACEI_workflow.codes.torchmd_net.utils import (
+    cargar_desde_TorchMDNet,
+    guardar_metricas,
+    obtener_indices_test
+)
+
 referencias = cargar_desde_TorchMDNet("transform/input_torchmd-net.h5")
 test_idx = obtener_indices_test("train/splits.npz")
 referencias = DinamicaMolecular.subconjunto(referencias, test_idx)
@@ -141,6 +150,8 @@ echo "Generación de datos para graficar finalizada correctamente"
 echo "Iniciando generación de Graficas"
 
 python - <<EOF
+from LIACEI_workflow.analysis.DinamicaMolecular import Graficas_genericas
+
 Graficas_genericas.grafica_epoch_vs_loss()
 
 Graficas_genericas.grafica_ref_vs_inf_energy()
@@ -153,35 +164,15 @@ Graficas_genericas.grafica_ref_vs_inf_energy_hist()
 
 Graficas_genericas.grafica_ref_vs_inf_energy_abs_dif()
 
-Graficas_genericas.grafica_ref_vs_inf_forces_angles_abs_dif_hydrogen()
+Graficas_genericas.graficas_ref_vs_inf_forces_angles_abs_dif()
 
-Graficas_genericas.grafica_ref_vs_inf_forces_angles_abs_dif_oxygen()
+Graficas_genericas.graficas_ref_vs_inf_forces_modules()
 
-Graficas_genericas.grafica_ref_vs_inf_forces_angles_abs_dif_aluminium()
+Graficas_genericas.graficas_ref_vs_inf_forces_modules_hist_2d()
 
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_hydrogen()
+Graficas_genericas.graficas_ref_vs_inf_forces_modules_hist_3d()
 
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_hydrogen_hist_2d()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_hydrogen_hist_3d()
-
-Graficas_genericas.graficar_ref_vs_inf_forces_modules_hydrogen_hist()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_oxygen()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_oxygen_hist_2d()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_oxygen_hist_3d()
-
-Graficas_genericas.graficar_ref_vs_inf_forces_modules_oxygen_hist()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_aluminium()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_aluminium_hist_2d()
-
-Graficas_genericas.grafica_ref_vs_inf_forces_modules_aluminium_hist_3d()
-
-Graficas_genericas.graficar_ref_vs_inf_forces_modules_aluminium_hist()
+Graficas_genericas.graficas_ref_vs_inf_forces_modules_hist()
 EOF
 
 if [[ $? -ne 0 ]]; then
